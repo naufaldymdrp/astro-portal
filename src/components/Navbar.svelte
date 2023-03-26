2487285bfe89dbb5d553f3577d6a50793d0e22bd
Naufaldy Rooswandy <naufaldygood@outlook.com>
2023-03-26 23:11:33 +0700

    Updated astro to use the latest
    Decided to delete all and re-create astro project using latest update

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<div class="navbar">
+    <div class="navbar-banner">Astro Portal</div>
+    <nav class="navbar-nav">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a >Warehouse Data</a></li>
+            <li><a >About</a></li>
+            <li><a >Privacy</a></li>
+        </ul>
+    </nav>
+</div>
+
+<style>
+    .navbar {
+        border: 1px solid black;
+        box-sizing: border-box;
+    }
+
+    .navbar-nav ul {
+        list-style-type: none;
+    }
+</style>
